2c99bf78cd693b1fd4a640f82731df286d0ec408
mateuszmilczarek <matmil92@outlook.com>
2025-10-04 19:14:01 +0200

    fix

--- GoodVibes.Traffic.Domain/Alert.cs
+++ GoodVibes.Traffic.Domain/Alert.cs
@@ -2,8 +2,7 @@ namespace GoodVibes.Traffic.Domain;
 
 public class Alert
 {
-    public float? LAT { get; set; }
-    public float? LON { get; set; }
     public string ALERT_TYPE { get; set; }
     public string SHIP_ID { get; set; }
+    public string REASON { get; set; }
 }
