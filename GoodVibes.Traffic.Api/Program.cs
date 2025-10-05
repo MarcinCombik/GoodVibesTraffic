f0205af0e06efa64d509b640a0dbad6030f88a72
MarcinC <marcin.combik@kombikit>
2025-10-05 11:28:05 +0200

    Tune

--- GoodVibes.Traffic.Api/Program.cs
+++ GoodVibes.Traffic.Api/Program.cs
@@ -219,7 +219,7 @@ app.MapPost("/OpenAiApiRequest/Alerts", async (IOpenAiApiClient client) =>
 
     string fileContent = File.ReadAllText("ships-analitic.json");
 
-    int chunkSize = 80000;
+    int chunkSize = 100000;
     int totalLength = fileContent.Length;
     
 
